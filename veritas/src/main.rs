f14ec008db6a56f30b45405782311f5ddf238f22
Tanishq Dubey <tanishq.dubey@gmail.com>
2018-04-10 12:10:06 -0500

    Add a project initalizer and add coloring

--- veritas/src/main.rs
+++ veritas/src/main.rs
@@ -5,13 +5,15 @@ extern crate git2;
 extern crate serde;
 extern crate serde_json;
 extern crate clap;
+extern crate colored;
 
 use std::path::Path;
-use std::fs::File;
-use std::fs::create_dir_all;
+use std::fs::{File, create_dir_all};
 use std::error::Error;
+use std::io::{Write, BufWriter, stdin, stdout};
 use git2::build::{ RepoBuilder };
 use clap::{Arg, App};
+use colored::*;
 
 /*
  * A dependency only needs a name, which is in the format "username/git-repo". The latest master
@@ -40,7 +42,7 @@ struct Package {
 }
 
 fn get_repo(repo: &str, clone_path: &str) {
-    println!("Getting dependency {}...", repo);
+    println!("{} {}","Getting dependency:".green().bold(), repo);
 
     let mut builder = RepoBuilder::new();
 
@@ -58,7 +60,7 @@ fn get_dependencies(current_path: &str, package: &Dependency) {
     // Check if veri_modules directory exists, otherwise, make it
     let name = package.name.split("/").collect::<Vec<&str>>()[1];
     let path_name = format!("{}/veri_modules/{}", current_path, name);
-    create_dir_all(&path_name);
+    create_dir_all(&path_name).unwrap();
     // Clone our first dependency in the veri_modules directory, with the name of the module
     get_repo(&package.name, &path_name);
     // get_dependencies of the package we cloned
@@ -74,10 +76,73 @@ fn get_dependencies(current_path: &str, package: &Dependency) {
  * Create a project in the current directory
  */
 fn init_project() {
+    // Ask the user for information
+    println!("{}", "Creating a new verilog project...".yellow().bold());
 
+    print!("{}", "Please enter the project name: ".yellow().bold());
+    stdout().flush().unwrap();
+    let mut nme = String::new();
+    stdin().read_line(&mut nme).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+    nme = nme.trim().to_string();
+    while nme.chars().count() == 0 {
+        print!("{}", "Name is invalid, please try again: ".red().bold());
+        stdout().flush().unwrap();
+        stdin().read_line(&mut nme).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+        nme = nme.trim().to_string();
+    }
+
+    print!("{}", "Please enter a short project description: ".yellow().bold());
+    stdout().flush().unwrap();
+    let mut desc = String::new();
+    stdin().read_line(&mut desc).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+    desc = desc.trim().to_string();
+    while desc.chars().count() == 0 {
+        print!("{}", "Description is invalid, please try again: ".red().bold());
+        stdout().flush().unwrap();
+        stdin().read_line(&mut desc).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+        desc = desc.trim().to_string();
+    }
+
+    print!("{}", "Please enter a project version (Default: 0.1.0): ".yellow().bold());
+    stdout().flush().unwrap();
+    let mut ver = String::new();
+    stdin().read_line(&mut ver).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+    ver = ver.trim().to_string();
+    if ver.chars().count() == 0 {
+        ver = "0.1.0".to_string();
+    }
+
+    print!("{}", "Please enter your name (Default: \"\"): ".yellow().bold());
+    stdout().flush().unwrap();
+    let mut rname = String::new();
+    stdin().read_line(&mut rname).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+    rname = rname.trim().to_string();
+    if rname.chars().count() == 0 {
+        rname = "".to_string();
+    }
+
+    print!("{}", "Please enter a license (Default: MIT): ".yellow().bold());
+    stdout().flush().unwrap();
+    let mut lic = String::new();
+    stdin().read_line(&mut lic).ok().expect(&format!("{}", "Couldn't read line".red().bold()));
+    lic = lic.trim().to_string();
+    if lic.chars().count() == 0 {
+        lic = "MIT".to_string();
+    }
+
+    // Create the json
+    let package = Package {name: nme, version: ver, description: desc, dependencies: Some(Vec::new()), author: Some(rname), license: Some(lic)};
+    let j = serde_json::to_string_pretty(&package).unwrap();
+
+    let f = File::create("./package.json").expect("Unable to create file");
+    let mut f = BufWriter::new(f);
+    f.write_all(j.as_bytes()).expect("Unable to write data");
+
+    println!("{}", "Project created! Begin by adding dependencies to your package.json.".green().bold())
 }
 
 fn main() {
+    // Create help, version, and validation
     let matches = App::new("Veritas")
         .version("1.0")
         .author("Tanishq Dubey <tanishq.dubey@gmail.com>")
@@ -90,9 +155,12 @@ fn main() {
 
     match matches.value_of("COMMAND").unwrap() {
         "init" => {
-            println!("Not Implemented yet...")
+            // Create a project
+            init_project();
         },
         "install" => {
+            // Install dependencies
+            // TODO: Make a lock file
             let p = read_package_json(&"./package.json").unwrap();
             let deps = p.dependencies.unwrap_or(Vec::new());
             // repeat
@@ -101,7 +169,8 @@ fn main() {
             }
         },
         "update" => {
-            println!("Not Implemented yet...")
+            // Update dependencies
+            println!("Not Implemented yet...");
         },
         _ => unreachable!()
     }
